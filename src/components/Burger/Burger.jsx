89d8c70293e6b6a1e7836d970ec75732e290e4d4
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-07-24 20:49:52 +0800

    feat: add BurgerModal & BurgerMenu

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
 import styles from './Burger.module.scss'
 const { burgerContainer, bar } = styles
 
-const Burgur = () => {
-  const [burgerExpanded, setBurgerExpanded] = useState(false)
+const Burgur = ({ burgerToggle, onBurgerToggle }) => {
   return (
-    <div className={burgerContainer}>
-      <div
-        className={bar}
-        data-expanded={burgerExpanded}
-        onClick={() => setBurgerExpanded((pre) => !pre)}
-      ></div>
+    <div className={burgerContainer} onClick={() => onBurgerToggle()}>
+      <div className={bar} data-phase={burgerToggle}></div>
     </div>
   )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,30 +1,38 @@
 import styles from './Nav.module.scss'
-import Burgur from 'components/Burger/Burger'
 import { ReactComponent as IconLogo } from 'assets/icons/icon-logo.svg'
 import { ReactComponent as IconAdd } from 'assets/icons/icon-add.svg'
 import { ReactComponent as IconNotification } from 'assets/icons/icon-notification.svg'
 import { ReactComponent as IconUser } from 'assets/icons/icon-user.svg'
-const { navContainer, left, right, user, burger } = styles
+import BurgerModal from 'components/BurgerModal/BurgerModal'
+const {
+  navContainer,
+  left,
+  right,
+  rightListItem,
+  rightListIcon,
+  user,
+  burger
+} = styles
 
 const Nav = () => {
   return (
     <div className={navContainer}>
       <div className={left}>
         <div className={burger}>
-          <Burgur />
+          <BurgerModal />
         </div>
         <IconLogo />
         <span>登山小站</span>
       </div>
       <div className={right}>
-        <div>找路徑</div>
-        <div>找心得</div>
+        <div className={rightListItem}>找路徑</div>
+        <div className={rightListItem}>找心得</div>
         <button>
           <IconAdd />
           我的心得
         </button>
-        <IconNotification />
-        <IconUser className={user} />
+        <IconNotification className={rightListIcon} />
+        <IconUser className={`${user} ${rightListIcon}`} />
       </div>
     </div>
   )
