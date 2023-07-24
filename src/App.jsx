6572e334bb9239d4390cda44149c151fbe65d654
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-07-24 13:50:43 +0800

    fix: modified eslint & prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import 'styles/reset.scss'
 import 'styles/base.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import MainPage from "pages/MainPage/MainPage"
-import LoginPage from "pages/LoginPage/LoginPage"
-import TrailsPage from "pages/TrailsPage/TrailsPage"
-import SingleTrailPage from "pages/SingleTrailPage/SingleTrailPage"
-import ReviewsPage from "pages/ReviewsPage/ReviewsPage"
-import SingleReviewPage from "pages/SingleReviewPage/SingleReviewPage"
-import EditReviewPage from "pages/EditReviewPage/EditReviewPage"
-import UserPage from "pages/UserPage/UserPage"
-import AdminLoginPage from "pages/AdminLoginPage/AdminLoginPage"
-import AdminMainPage from "pages/AdminMainPage/AdminMainPage"
-import ErrorPage from "pages/ErrorPage/ErrorPage"
-
-
+import MainPage from 'pages/MainPage/MainPage'
+import LoginPage from 'pages/LoginPage/LoginPage'
+import TrailsPage from 'pages/TrailsPage/TrailsPage'
+import SingleTrailPage from 'pages/SingleTrailPage/SingleTrailPage'
+import ReviewsPage from 'pages/ReviewsPage/ReviewsPage'
+import SingleReviewPage from 'pages/SingleReviewPage/SingleReviewPage'
+import EditReviewPage from 'pages/EditReviewPage/EditReviewPage'
+import UserPage from 'pages/UserPage/UserPage'
+import AdminLoginPage from 'pages/AdminLoginPage/AdminLoginPage'
+import AdminMainPage from 'pages/AdminMainPage/AdminMainPage'
+import ErrorPage from 'pages/ErrorPage/ErrorPage'
 
 function App() {
   return (
