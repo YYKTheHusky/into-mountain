c4c892f8475a85aebc603c89f3dc98affb93305f
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-07-26 12:54:21 +0800

    fix: modify nav button

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,6 +4,8 @@ import { ReactComponent as IconAdd } from 'assets/icons/icon-add.svg'
 import { ReactComponent as IconNotification } from 'assets/icons/icon-notification.svg'
 import { ReactComponent as IconUser } from 'assets/icons/icon-user.svg'
 import BurgerModal from 'components/BurgerModal/BurgerModal'
+import { OvalButtonSmall } from 'components/Button/Button'
+
 const {
   navContainer,
   left,
@@ -11,7 +13,8 @@ const {
   rightListItem,
   rightListIcon,
   user,
-  burger
+  burger,
+  postReviewButton
 } = styles
 
 const Nav = () => {
@@ -27,10 +30,11 @@ const Nav = () => {
       <div className={right}>
         <div className={rightListItem}>找路徑</div>
         <div className={rightListItem}>找心得</div>
-        <button>
-          <IconAdd />
-          我的心得
-        </button>
+        <div className={postReviewButton}>
+          <OvalButtonSmall>
+            <IconAdd /> <span>我的心得</span>
+          </OvalButtonSmall>
+        </div>
         <IconNotification className={rightListIcon} />
         <IconUser className={`${user} ${rightListIcon}`} />
       </div>
