7875b8965b937b3cec854280dcfb2daae264e0e8
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-07-27 11:49:02 +0800

    feat: add InfoCard Pad RWD

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -8,7 +8,8 @@ const {
   count,
   infoSet,
   countTitle,
-  countTitle2
+  countTitle2,
+  information
 } = styles
 
 const InfoCard = ({ data, onAcitveContent }) => {
@@ -18,30 +19,35 @@ const InfoCard = ({ data, onAcitveContent }) => {
         <div className={avatar}>
           <img src={data.avatar} alt="" />
         </div>
-        <div className={myName}>{data.name}</div>
-        <div className={aboutMe}>{data.introduction}</div>
-        <span></span>
-        <div className={count}>
-          <div>
-            <div className={countTitle}>關注</div>
-            <div onClick={() => onAcitveContent('following')}>
-              {data.followerCount}
+        <div className={information}>
+          <div className={myName}>{data.name}</div>
+          <div className={aboutMe}>{data.introduction}</div>
+          <span></span>
+          <div className={count}>
+            <div>
+              <div className={countTitle}>關注</div>
+              <div onClick={() => onAcitveContent('following')}>
+                {data.followerCount}
+              </div>
+            </div>
+            <div>
+              <div className={countTitle}>粉絲</div>
+              <div onClick={() => onAcitveContent('follower')}>
+                {data.followingCount}
+              </div>
+            </div>
+            <div>
+              <div className={countTitle2}>總讚數</div>
+              <div>{data.PostLikeCount}</div>
             </div>
           </div>
-          <div>
-            <div className={countTitle}>粉絲</div>
-            <div onClick={() => onAcitveContent('follower')}>
-              {data.followingCount}
-            </div>
+          <span></span>
+          <div
+            className={infoSet}
+            onClick={() => onAcitveContent('infoSetting')}
+          >
+            編輯個人資料
           </div>
-          <div>
-            <div className={countTitle2}>總讚數</div>
-            <div>{data.PostLikeCount}</div>
-          </div>
-        </div>
-        <span></span>
-        <div className={infoSet} onClick={() => onAcitveContent('infoSetting')}>
-          編輯個人資料
         </div>
       </div>
     </div>
