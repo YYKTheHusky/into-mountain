1f2317617311249c5b5fbabbe2499824e48d9eec
magic9701 <magic9701@gmail.com>
2023-07-29 01:54:19 +0800

    style: Move modal background to base.scss

--- src/components/Modal/ConfirmModal.jsx
+++ src/components/Modal/ConfirmModal.jsx
@@ -1,19 +1,34 @@
 // scss
-import { SecondaryButtonBright, SecondaryButtonGray } from 'components/Button/Button'
+import {
+  SecondaryButtonBright,
+  SecondaryButtonGray
+} from 'components/Button/Button'
 import styles from 'components/Modal/ConfirmModal.module.scss'
 
-// 父層設定開啟/關閉function
+// 父層設定一個State、兩個function，點選兩個按鈕做某些事情
 // const [ isModalOpen, setIsModalOpen] = useState(false)
-// const openModal = () => {
-//   setIsModalOpen(true)
+// const confirmFunction = () => {
+//   setIsModalOpen(false)
 // }
-// const closeModal = (event) => {
+// const cancelFunction = (event) => {
 //   if (event.target === event.currentTarget) {
 //     setIsModalOpen(false)
 //   }
 // }
+// 引用方式
+// <ConfirmModal
+//   isModalOpen={isModalOpen}
+//   message="確定要刪除嗎?"
+//   confirmFunction={confirmFunction}
+//   cancelFunction={cancelFunction}
+// ></ConfirmModal>
 
-export default function ConfirmModal({ isModalOpen, message, confirmFunction, cancelFunction }) {
+export default function ConfirmModal({
+  isModalOpen,
+  message,
+  confirmFunction,
+  cancelFunction
+}) {
   // 當isModalOpen為false，不會顯示modal
   // isModalOpen透過父層設state控制
   if (!isModalOpen) {
@@ -21,15 +36,19 @@ export default function ConfirmModal({ isModalOpen, message, confirmFunction, ca
   }
 
   return (
-    <div className={styles.background}>
+    <div className="modalBackground">
       <div className={styles.modalBody}>
         <h5>{message}</h5>
         <div className={styles.buttonGroup}>
           <div className={styles.buttonContainer}>
-            <SecondaryButtonBright onclick={confirmFunction}>確定</SecondaryButtonBright>
+            <SecondaryButtonBright onclick={confirmFunction}>
+              確定
+            </SecondaryButtonBright>
           </div>
           <div className={styles.buttonContainer}>
-            <SecondaryButtonGray onclick={cancelFunction}>取消</SecondaryButtonGray>
+            <SecondaryButtonGray onclick={cancelFunction}>
+              取消
+            </SecondaryButtonGray>
           </div>
         </div>
       </div>
