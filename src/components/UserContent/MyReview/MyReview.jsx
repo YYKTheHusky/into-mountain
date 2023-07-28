01833e7f9a67e760265708e1aa87023f192373a6
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-07-28 21:02:54 +0800

    refactor: modify MyReview Component

--- src/components/UserContent/MyReview/MyReview.jsx
+++ src/components/UserContent/MyReview/MyReview.jsx
@@ -1,66 +1,25 @@
 // import YouHaveNothing from 'components/UserContent/YouHaveNothing/YouHaveNothing'
 import styles from './MyReview.module.scss'
-const {
-  myReviewContainer,
-  listContainer,
-  listTitle,
-  listItem,
-  itemRight,
-  itemLeft,
-  itemRightTitle,
-  itemRightTime,
-  titleButton1,
-  titleButton2,
-  list
-} = styles
-
-const ListItem = () => {
-  return (
-    <div className={listItem}>
-      <div className={itemLeft}>
-        <img src="https://picsum.photos/200/300?grayscale" alt="" />
-      </div>
-      <div className={itemRight}>
-        <div className={itemRightTitle}>
-          文章標題
-          <div>
-            <button className={titleButton1}>按鈕1</button>
-            <button className={titleButton2}>按鈕2</button>
-          </div>
-        </div>
-        <p>
-          文章內文前100字文章內文前文章內文前文章內文前文章內文前文章內文前文章內文前文章內文前
-          文章內文前文章內文前文章內文前文章內文前文章內文前文章內文前文章內文前文章內文前文章內
-          文前文章內文前文章內文前文章內文前文章內文前
-        </p>
-        <div className={itemRightTime}>發表於：2023/7/23 20:23 </div>
-      </div>
-    </div>
-  )
-}
+import ListItem from './ListItem/ListItem'
+import RightSideContainer from 'components/UserContent/RightSideContainer/RightSideContainer'
+const { myReviewContainer } = styles
 
 const MyReview = () => {
   return (
-    <div className={myReviewContainer}>
-      {/* <div>
-        <YouHaveNothing robotTitle="心得" robotDescription="還未發佈過心得" />
-      </div> */}
-      <div className={listContainer}>
-        <div className={listTitle}>心得</div>
-        <div className={list}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-        </div>
+    <RightSideContainer title="心得">
+      <div className={myReviewContainer}>
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
       </div>
-    </div>
+    </RightSideContainer>
   )
 }
 export default MyReview
