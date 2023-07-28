f344c855df441e945d32c9aa97c18242bf97dae9
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-07-28 20:19:56 +0800

    style: modify UserPage RWD

--- src/components/UserContent/FollowCard/FollowCard.jsx
+++ src/components/UserContent/FollowCard/FollowCard.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import styles from './FollowCard.module.scss'
 import { SecondaryButton } from 'components/Button/Button'
-const { followerCardCointainer, cardAvatar, cardTitle, cardButton } = styles
+const { followerCardCointainer, cardHead, cardAvatar, cardTitle, cardButton } =
+  styles
 
 const FollowerCard = () => {
   const [isFollow, setIsFollow] = useState(false)
 
   return (
     <div className={followerCardCointainer}>
-      <div className={cardAvatar}>
-        <img src="https://picsum.photos/id/237/200/300" alt="" />
+      <div className={cardHead}>
+        <div className={cardAvatar}>
+          <img src="https://picsum.photos/id/237/200/300" alt="" />
+        </div>
+        <div className={cardTitle}>名稱</div>
       </div>
-      <div className={cardTitle}>名稱</div>
       <div className={cardButton}>
         <SecondaryButton
           onClick={() => {
