74f3d3fdaefa016b2eb3e2701fb4dbad02ed80c0
magic9701 <magic9701@gmail.com>
2023-08-11 08:33:25 +0800

    feat: add post api

--- src/pages/EditReviewPage/EditReviewPage.jsx
+++ src/pages/EditReviewPage/EditReviewPage.jsx
@@ -1,5 +1,6 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { useEffect, useRef, useState } from 'react'
+import Toast from 'utils/sweetAlertConfig.js'
 
 // scss
 import styles from 'pages/EditReviewPage/EditReviewPage.module.scss'
@@ -8,16 +9,18 @@ import styles from 'pages/EditReviewPage/EditReviewPage.module.scss'
 import MainLayout from 'components/MainLayout/MainLayout'
 import WholePageModal from 'components/Modal/WholePageModal'
 import IconRadioInput from 'components/Input/IconRadioInput'
-import {
-  SecondaryButton,
-  SecondaryButtonGray,
-  SecondaryButtonLight
-} from 'components/Button/Button'
+import { SecondaryButton, SecondaryButtonLight } from 'components/Button/Button'
 import ImgDropzone from 'components/Input/ImgDropzone'
+import UpLoadingSpinner from 'components/Spinner/UpLoadingSpinner'
+// api
+import { getOneTempPost, publishPost } from 'api/post'
 
 export default function EditReviewPage() {
   const location = useLocation()
+  const navigate = useNavigate()
+  const { reviewID } = useParams()
   const [isNewPost, setIsNewPost] = useState(null)
+  const [isLoading, setIsLoading] = useState('')
   const formRef = useRef(null)
   // 輸入內容state
   const [title, setTitle] = useState('')
@@ -25,6 +28,7 @@ export default function EditReviewPage() {
   const [difficulty, setDifficulty] = useState(null)
   const [recommend, setRecommend] = useState(null)
   const [uploadedImage, setUploadedImage] = useState(null)
+  const [imgForAPI, setImgForAPI] = useState(null)
 
   // 困難度與推薦度
   const handleDifficultyChange = (number) => {
@@ -44,115 +48,158 @@ export default function EditReviewPage() {
       setUploadedImage(reader.result)
     }
     reader.readAsDataURL(file)
+    setImgForAPI(file)
   }
 
-  const handleFormSubmit = (event) => {
+  // 發布功能
+  const handleFormSubmit = async (event) => {
     event.preventDefault()
     const formData = new FormData(formRef.current)
     const formValues = Object.fromEntries(formData.entries())
     formValues.difficulty = difficulty
     formValues.recommend = recommend
-    formValues.image = uploadedImage
+    formValues.image = imgForAPI
 
     // 檢查輸入內容
-    const checkInput = () => {
-      const whitespaceRegex = /^\s*$/
-      if (whitespaceRegex.test(title)) {
-        console.log('請輸入標題')
-      } else if (!formValues.category) {
-        console.log('請選擇心得分類')
-      } else if (!difficulty) {
-        console.log('請選擇困難度1-5分')
-      } else if (!recommend) {
-        console.log('請選擇推薦指數1-5分')
-      } else if (!uploadedImage) {
-        console.log('請上傳一張封面圖片')
-      } else if (whitespaceRegex.test(description)) {
-        console.log('請輸入內容')
+    const whitespaceRegex = /^\s*$/
+    const isNotValid =
+      whitespaceRegex.test(title) ||
+      !formValues.category ||
+      !difficulty ||
+      !recommend ||
+      !uploadedImage ||
+      whitespaceRegex.test(description)
+
+    if (isNotValid) {
+      Toast.fire({
+        icon: 'info',
+        title: '請確認每個項目都有輸入!'
+      })
+    } else {
+      setIsLoading('上傳中')
+      const { success } = await publishPost(formValues)
+      if (success) {
+        setIsLoading('')
+        Toast.fire({
+          icon: 'success',
+          title: '發布成功!'
+        })
+        navigate('/review')
+      } else if (!success) {
+        setIsLoading('')
+        Toast.fire({
+          icon: 'error',
+          title: '發布失敗!'
+        })
       }
     }
-
-    // 在這裡進行發布心得的相關邏輯
-    console.log(formValues)
-    checkInput()
   }
 
   useEffect(() => {
-    if (location.pathname === '/newReview') {
-      setIsNewPost(true)
+    const editOrNew = async () => {
+      if (location.pathname === '/newReview') {
+        setIsNewPost(true)
+      } else {
+        setIsNewPost(false)
+        setIsLoading('讀取中')
+        const { postData } = await getOneTempPost(reviewID)
+        setTitle(postData.title)
+        setDescription(postData.description)
+        setUploadedImage(postData.image)
+        setIsLoading('')
+      }
     }
+    editOrNew()
   }, [location])
 
   return (
-    <MainLayout>
-      <WholePageModal>
-        <h3 className={styles.title}>{isNewPost ? '新增心得' : '編輯心得'}</h3>
-        <form className={styles.form} ref={formRef} onSubmit={handleFormSubmit}>
-          {/* 心得標題 */}
-          <label htmlFor="title" className={styles.inputLabel}>
-            心得標題
-          </label>
-          <input
-            type="text"
-            maxLength="40"
-            id="title"
-            name="title"
-            value={title}
-            placeholder="請輸入標題"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <div className={styles.titleLength}>{title.length} / 40</div>
-          {/* 心得分類 */}
-          <label className={styles.inputLabel}>心得分類</label>
-          <div>
-            <input type="radio" id="baiyue" name="category" value="百岳" />
-            <label htmlFor="baiyue">百岳</label>
-            <input type="radio" id="subur" name="category" value="郊山" />
-            <label htmlFor="subur">郊山</label>
-            <input type="radio" id="oversea" name="category" value="海外" />
-            <label htmlFor="oversea">海外</label>
-          </div>
-          {/* 困難度 */}
-          <label className={styles.inputLabel}>困難度</label>
-          <IconRadioInput
-            iconType="difficulty"
-            onChange={handleDifficultyChange}
-          />
-          {/* 推薦指數 */}
-          <label className={styles.inputLabel}>推薦指數</label>
-          <IconRadioInput
-            iconType="recommend"
-            onChange={handleRecommendChange}
-          />
-          {/* 上傳封面圖片 */}
-          <label className={styles.inputLabel}>上傳封面圖片</label>
-          <ImgDropzone uploadedImage={uploadedImage} onDrop={handleDrop} />
-          {/* 心得內容 */}
-          <label className={styles.inputLabel}>心得內容</label>
-          <textarea
-            maxLength="5000"
-            placeholder="請輸入心得內容"
-            name="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <div className={styles.descriptionLength}>
-            {description.length} / 5000
-          </div>
-          {/* 按鈕 */}
-          <div className={styles.buttonGroup}>
-            <div className={styles.buttonContainer}>
-              <SecondaryButtonGray>取消</SecondaryButtonGray>
+    <div>
+      {isLoading !== '' && <UpLoadingSpinner text={isLoading} />}
+      <MainLayout>
+        <WholePageModal>
+          <h3 className={styles.title}>
+            {isNewPost ? '新增心得' : '編輯心得'}
+          </h3>
+          <form
+            className={styles.form}
+            ref={formRef}
+            onSubmit={handleFormSubmit}
+          >
+            {/* 心得標題 */}
+            <label htmlFor="title" className={styles.inputLabel}>
+              心得標題
+            </label>
+            <input
+              type="text"
+              maxLength="40"
+              id="title"
+              name="title"
+              value={title}
+              placeholder="請輸入標題"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <div className={styles.titleLength}>{title.length} / 40</div>
+            {/* 心得分類 */}
+            <label className={styles.inputLabel}>心得分類</label>
+            <div>
+              <input type="radio" id="baiyue" name="category" value="百岳" />
+              <label htmlFor="baiyue">百岳</label>
+              <input type="radio" id="subur" name="category" value="郊山" />
+              <label htmlFor="subur">郊山</label>
+              <input type="radio" id="oversea" name="category" value="海外" />
+              <label htmlFor="oversea">海外</label>
             </div>
-            <div className={styles.buttonContainer}>
-              <SecondaryButtonLight>暫存心得</SecondaryButtonLight>
+            {/* 困難度 */}
+            <label className={styles.inputLabel}>困難度</label>
+            <IconRadioInput
+              iconType="difficulty"
+              onChange={handleDifficultyChange}
+            />
+            {/* 推薦指數 */}
+            <label className={styles.inputLabel}>推薦指數</label>
+            <IconRadioInput
+              iconType="recommend"
+              onChange={handleRecommendChange}
+            />
+            {/* 上傳封面圖片 */}
+            <label className={styles.inputLabel}>上傳封面圖片</label>
+            <ImgDropzone uploadedImage={uploadedImage} onDrop={handleDrop} />
+            {/* 心得內容 */}
+            <label className={styles.inputLabel}>心得內容</label>
+            <textarea
+              maxLength="5000"
+              placeholder="請輸入心得內容"
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <div className={styles.descriptionLength}>
+              {description.length} / 5000
             </div>
-            <div className={styles.buttonContainer}>
-              <SecondaryButton type="submit">發布心得</SecondaryButton>
+            {/* 按鈕 */}
+            <div className={styles.buttonGroup}>
+              {isNewPost ? (
+                <>
+                  <div className={styles.buttonContainer}>
+                    <SecondaryButtonLight>暫存心得</SecondaryButtonLight>
+                  </div>
+                  <div className={styles.buttonContainer}>
+                    <SecondaryButton onClick={handleFormSubmit}>
+                      發布心得
+                    </SecondaryButton>
+                  </div>
+                </>
+              ) : (
+                <div className={styles.buttonContainer}>
+                  <SecondaryButton onClick={handleFormSubmit}>
+                    編輯心得
+                  </SecondaryButton>
+                </div>
+              )}
             </div>
-          </div>
-        </form>
-      </WholePageModal>
-    </MainLayout>
+          </form>
+        </WholePageModal>
+      </MainLayout>
+    </div>
   )
 }
--- src/pages/SingleTrailPage/SingleTrailPage.jsx
+++ src/pages/SingleTrailPage/SingleTrailPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
 // scss
 import styles from 'pages/SingleTrailPage/SingleTrailPage.module.scss'
 
@@ -16,12 +16,16 @@ import MapTable from 'components/TrailsInformation/MapTable.jsx'
 import MainLayout from 'components/MainLayout/MainLayout'
 import ReportModal from 'components/Modal/ReportModal'
 import WholePageModal from 'components/Modal/WholePageModal'
+// api
+import { getOneTrail } from 'api/trail'
 
 export default function SingleTrailPage() {
   const location = useLocation()
   const navigate = useNavigate()
+  const { trailID } = useParams()
   const [activeTab, setActiveTab] = useState(null)
   const [isReportModalOpen, setIsReportModalOpen] = useState(false)
+  const [data, setData] = useState(null)
 
   useEffect(() => {
     if (location.pathname.includes('detail')) {
@@ -30,101 +34,103 @@ export default function SingleTrailPage() {
       setActiveTab('gpx')
     }
   }, [navigate])
+
+  useEffect(() => {
+    const getData = async () => {
+      const { trailData } = await getOneTrail(trailID)
+      setData(trailData)
+    }
+    getData()
+  }, [])
+
   return (
     <MainLayout>
-      <WholePageModal>
-        {/* 標題、圖片、步道回報 */}
-        <section className={styles.trailNameAndPhoto}>
-          <div className={styles.nameAndLike}>
-            <h2 className={styles.title}>步道的名字字字字</h2>
-            <div className={styles.socialButtons}>
-              <div className={`cursor-point ${styles.favorite}`}>
-                <img
-                  className={styles.icon}
-                  src={defaultFavorite}
-                  alt="defaultFavorite"
-                ></img>
-                <span>收藏</span>
-              </div>
-              <div className={`cursor-point ${styles.share}`}>
-                <img
-                  className={styles.icon}
-                  src={shareIcon}
-                  alt="shareIcon"
-                ></img>
-                <span>分享</span>
-              </div>
-            </div>
-          </div>
-          <div className={styles.photoAndReport}>
-            <div className={styles.photoContainer}>
-              <img className={styles.photo} src={photo} alt="步道圖片片片片" />
-            </div>
-            <div className={styles.reportContainer}>
-              <div className={styles.reportHeader}>
-                <h4>路況回報</h4>
-                <div className={styles.reportButton}>
-                  <SecondaryButton onClick={() => setIsReportModalOpen(true)}>
-                    我要回報
-                  </SecondaryButton>
+      {data && (
+        <WholePageModal>
+          {/* 標題、圖片、步道回報 */}
+          <section className={styles.trailNameAndPhoto}>
+            <div className={styles.nameAndLike}>
+              <h2 className={styles.title}>{data.title}</h2>
+              <div className={styles.socialButtons}>
+                <div className={`cursor-point ${styles.favorite}`}>
+                  <img
+                    className={styles.icon}
+                    src={defaultFavorite}
+                    alt="defaultFavorite"
+                  ></img>
+                  <span>收藏</span>
                 </div>
-                <ReportModal
-                  isReportModalOpen={isReportModalOpen}
-                  setIsReportModalOpen={setIsReportModalOpen}
-                ></ReportModal>
-              </div>
-              <div className={styles.reports}>
-                <TrailReport></TrailReport>
-                <TrailReport></TrailReport>
-                <TrailReport></TrailReport>
-                <TrailReport></TrailReport>
-                <TrailReport></TrailReport>
+                <div className={`cursor-point ${styles.share}`}>
+                  <img
+                    className={styles.icon}
+                    src={shareIcon}
+                    alt="shareIcon"
+                  ></img>
+                  <span>分享</span>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-        {/* 基本介紹 */}
-        <section className={styles.trailIntroduction}>
-          <h4>路線介紹</h4>
-          <span>
-            這是一篇假的文章，這是一篇假的文章，這是一篇假的文章，這是一篇假的文章，
-            這是一篇假的文章，這是一篇假的文章，
-            這是一篇假的文章，這是一篇假的文章，
-            這是一篇假的文章，這是一篇假的文章，
-            這是一篇假的文章，這是一篇假的文章，
-            這是一篇假的文章，這是一篇假的文章， 這是一篇假的文章，
-            這是一篇假的文章，
-            這是一篇假的文章，這是一篇假的文章，這是一篇假的文章，
-          </span>
-        </section>
-        {/* 詳細資料 */}
-        <section className={styles.trailDetail}>
-          <div className={styles.tabs}>
-            <Link to={`/trail/trailID/detail`}>
-              <div
-                className={`cursor-point ${styles.tabOne} ${
-                  activeTab === 'detail' && styles.active
-                }`}
-              >
-                <h4>基本資訊</h4>
+            <div className={styles.photoAndReport}>
+              <div className={styles.photoContainer}>
+                <img className={styles.photo} src={photo} alt="步道圖片" />
               </div>
-            </Link>
-            <Link to={`/trail/trailID/gpx`}>
-              <div
-                className={`cursor-point ${styles.tabTwo} ${
-                  activeTab === 'gpx' && styles.active
-                }`}
-              >
-                <h4>路線地圖</h4>
+              <div className={styles.reportContainer}>
+                <div className={styles.reportHeader}>
+                  <h4>路況回報</h4>
+                  <div className={styles.reportButton}>
+                    <SecondaryButton onClick={() => setIsReportModalOpen(true)}>
+                      我要回報
+                    </SecondaryButton>
+                  </div>
+                  <ReportModal
+                    isReportModalOpen={isReportModalOpen}
+                    setIsReportModalOpen={setIsReportModalOpen}
+                  />
+                </div>
+                <div className={styles.reports}>
+                  <TrailReport></TrailReport>
+                  <TrailReport></TrailReport>
+                  <TrailReport></TrailReport>
+                  <TrailReport></TrailReport>
+                  <TrailReport></TrailReport>
+                </div>
               </div>
-            </Link>
-          </div>
-          <div className={styles.information}>
-            {activeTab === 'detail' && <InformationTable></InformationTable>}
-            {activeTab === 'gpx' && <MapTable></MapTable>}
-          </div>
-        </section>
-      </WholePageModal>
+            </div>
+          </section>
+          {/* 基本介紹 */}
+          <section className={styles.trailIntroduction}>
+            <h4>路線介紹</h4>
+            <span>{data.introduction}</span>
+          </section>
+          {/* 詳細資料 */}
+          <section className={styles.trailDetail}>
+            <div className={styles.tabs}>
+              <Link to={`/trail/trailID/detail`}>
+                <div
+                  className={`cursor-point ${styles.tabOne} ${
+                    activeTab === 'detail' && styles.active
+                  }`}
+                >
+                  <h4>基本資訊</h4>
+                </div>
+              </Link>
+              <Link to={`/trail/trailID/gpx`}>
+                <div
+                  className={`cursor-point ${styles.tabTwo} ${
+                    activeTab === 'gpx' && styles.active
+                  }`}
+                >
+                  <h4>路線地圖</h4>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.information}>
+              {activeTab === 'detail' && <InformationTable data={data} />}
+              {activeTab === 'gpx' && <MapTable />}
+            </div>
+          </section>
+        </WholePageModal>
+      )}
     </MainLayout>
   )
 }
