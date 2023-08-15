8a662a07564e45e5e4bacb3a6a711aa8af6c453e
magic9701 <magic9701@gmail.com>
2023-08-15 11:02:42 +0800

    feat: add download function

--- src/components/TrailsInformation/MapTable.jsx
+++ src/components/TrailsInformation/MapTable.jsx
@@ -3,8 +3,19 @@ import styles from 'components/TrailsInformation/InformationTable.module.scss'
 // component
 import { SecondaryButton } from 'components/Button/Button'
 import Map from './Map'
+import { getTrailsGPX } from 'api/trail'
 
 export default function InformationTable({ data }) {
+  // 下載gpx
+  const handleDownload = async () => {
+    const { gpx } = await getTrailsGPX(data.id)
+    const blob = new Blob([gpx], { type: 'application/gpx+xml' })
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(blob)
+    link.download = `${data.title}.gpx`
+
+    link.click()
+  }
   return (
     <div className={styles.tableContainer}>
       <table className={styles.tableOne}>
@@ -24,7 +35,9 @@ export default function InformationTable({ data }) {
           <th>地圖</th>
           <td>
             <div className={styles.buttonContainer}>
-              <SecondaryButton>下載GPX</SecondaryButton>
+              <SecondaryButton onClick={handleDownload}>
+                下載GPX
+              </SecondaryButton>
             </div>
             <div className={styles.mapContainer}>
               <Map gpx={data.gpx} />
