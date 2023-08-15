6a23b3bda989f7bb572ceec92d01c9def0088639
magic9701 <magic9701@gmail.com>
2023-08-15 11:03:35 +0800

    feat: add edit and draft

--- src/pages/EditReviewPage/EditReviewPage.jsx
+++ src/pages/EditReviewPage/EditReviewPage.jsx
@@ -13,13 +13,14 @@ import { SecondaryButton, SecondaryButtonLight } from 'components/Button/Button'
 import ImgDropzone from 'components/Input/ImgDropzone'
 import UpLoadingSpinner from 'components/Spinner/UpLoadingSpinner'
 // api
-import { getOneTempPost, publishPost } from 'api/post'
+import { editPost, getPostForEdit, publishPost, scratchPost } from 'api/post'
 
 export default function EditReviewPage() {
   const location = useLocation()
   const navigate = useNavigate()
   const { reviewID } = useParams()
   const [isNewPost, setIsNewPost] = useState(null)
+  const [isDraft, setIsDraft] = useState(null)
   const [isLoading, setIsLoading] = useState('')
   const formRef = useRef(null)
   // 輸入內容state
@@ -28,7 +29,8 @@ export default function EditReviewPage() {
   const [difficulty, setDifficulty] = useState(null)
   const [recommend, setRecommend] = useState(null)
   const [uploadedImage, setUploadedImage] = useState(null)
-  const [imgForAPI, setImgForAPI] = useState(null)
+  const [image, setImage] = useState(null)
+  const [category, setCategory] = useState(null)
 
   // 困難度與推薦度
   const handleDifficultyChange = (number) => {
@@ -48,28 +50,31 @@ export default function EditReviewPage() {
       setUploadedImage(reader.result)
     }
     reader.readAsDataURL(file)
-    setImgForAPI(file)
+    setImage(file)
+  }
+
+  // 送出前檢查項目
+  const isNotValid =
+    /^\s*$/.test(title) ||
+    !category ||
+    !difficulty ||
+    !recommend ||
+    !uploadedImage ||
+    /^\s*$/.test(description)
+
+  const postData = {
+    title,
+    category,
+    description,
+    image,
+    difficulty,
+    recommend
   }
 
   // 發布功能
   const handleFormSubmit = async (event) => {
     event.preventDefault()
-    const formData = new FormData(formRef.current)
-    const formValues = Object.fromEntries(formData.entries())
-    formValues.difficulty = difficulty
-    formValues.recommend = recommend
-    formValues.image = imgForAPI
-
-    // 檢查輸入內容
-    const whitespaceRegex = /^\s*$/
-    const isNotValid =
-      whitespaceRegex.test(title) ||
-      !formValues.category ||
-      !difficulty ||
-      !recommend ||
-      !uploadedImage ||
-      whitespaceRegex.test(description)
-
+    // 檢查輸入內容無空白
     if (isNotValid) {
       Toast.fire({
         icon: 'info',
@@ -77,19 +82,106 @@ export default function EditReviewPage() {
       })
     } else {
       setIsLoading('上傳中')
-      const { success } = await publishPost(formValues)
+
+      // 區別上傳新文章 或 舊文章，call 不同的API
+      if (isNewPost) {
+        // 發布新文章
+        const { postId } = await publishPost(postData)
+        if (postId) {
+          setIsLoading('')
+          Toast.fire({
+            icon: 'success',
+            title: '發布成功!'
+          })
+          navigate(`/review/${postId}`)
+        } else if (!postId) {
+          setIsLoading('')
+          Toast.fire({
+            icon: 'error',
+            title: '發布失敗!'
+          })
+        }
+      } else if (!isNewPost) {
+        // 編輯舊文章，編輯草稿後發布
+        const { success } = await editPost({
+          reviewID,
+          ...postData,
+          inProgress: false
+        })
+        if (success) {
+          setIsLoading('')
+          Toast.fire({
+            icon: 'success',
+            title: '編輯成功!'
+          })
+          navigate(`/review/${reviewID}`)
+        } else if (!success) {
+          setIsLoading('')
+          Toast.fire({
+            icon: 'error',
+            title: '編輯失敗!'
+          })
+        }
+      }
+    }
+  }
+
+  // 編輯草稿
+  const handleEditScratch = async (event) => {
+    event.preventDefault()
+    // 檢查輸入內容無空白
+    if (isNotValid) {
+      Toast.fire({
+        icon: 'info',
+        title: '請確認每個項目都有輸入!'
+      })
+    } else {
+      setIsLoading('上傳中')
+      const { success } = await editPost({
+        reviewID,
+        ...postData,
+        inProgress: true
+      })
       if (success) {
         setIsLoading('')
         Toast.fire({
           icon: 'success',
-          title: '發布成功!'
+          title: '編輯成功!'
         })
-        navigate('/review')
+        navigate(`/review/${reviewID}`)
       } else if (!success) {
         setIsLoading('')
         Toast.fire({
           icon: 'error',
-          title: '發布失敗!'
+          title: '編輯失敗!'
+        })
+      }
+    }
+  }
+
+  // 暫存心得
+  const handleScratch = async (event) => {
+    event.preventDefault()
+    if (isNotValid) {
+      Toast.fire({
+        icon: 'info',
+        title: '請確認每個項目都有輸入!'
+      })
+    } else {
+      setIsLoading('上傳中')
+      const { postId } = await scratchPost(postData)
+      if (postId) {
+        setIsLoading('')
+        Toast.fire({
+          icon: 'success',
+          title: '暫存心得成功!'
+        })
+        navigate(`/review/${postId}`)
+      } else if (!postId) {
+        setIsLoading('')
+        Toast.fire({
+          icon: 'error',
+          title: '暫存心得失敗!'
         })
       }
     }
@@ -102,11 +194,25 @@ export default function EditReviewPage() {
       } else {
         setIsNewPost(false)
         setIsLoading('讀取中')
-        const { postData } = await getOneTempPost(reviewID)
-        setTitle(postData.title)
-        setDescription(postData.description)
-        setUploadedImage(postData.image)
-        setIsLoading('')
+        const { postData } = await getPostForEdit(reviewID)
+        if (postData) {
+          if (postData.inProgress === false) {
+            setIsDraft(false)
+          } else if (postData.inProgress === true) {
+            setIsDraft(true)
+          }
+          setTitle(postData.title)
+          setDescription(postData.description)
+          setUploadedImage(postData.image)
+          setIsLoading('')
+        } else {
+          Toast.fire({
+            icon: 'error',
+            title: '您無法修改此篇文章!'
+          })
+          setIsLoading('')
+          navigate('/search/allReviews')
+        }
       }
     }
     editOrNew()
@@ -118,13 +224,9 @@ export default function EditReviewPage() {
       <MainLayout>
         <WholePageModal>
           <h3 className={styles.title}>
-            {isNewPost ? '新增心得' : '編輯心得'}
+            {isNewPost ? '新增心得' : isDraft ? '編輯草稿' : '編輯心得'}
           </h3>
-          <form
-            className={styles.form}
-            ref={formRef}
-            onSubmit={handleFormSubmit}
-          >
+          <form className={styles.form} ref={formRef}>
             {/* 心得標題 */}
             <label htmlFor="title" className={styles.inputLabel}>
               心得標題
@@ -143,11 +245,17 @@ export default function EditReviewPage() {
             <label className={styles.inputLabel}>心得分類</label>
             <div>
               <input type="radio" id="baiyue" name="category" value="百岳" />
-              <label htmlFor="baiyue">百岳</label>
+              <label htmlFor="baiyue" onClick={(e) => setCategory('百岳')}>
+                百岳
+              </label>
               <input type="radio" id="subur" name="category" value="郊山" />
-              <label htmlFor="subur">郊山</label>
+              <label htmlFor="subur" onClick={(e) => setCategory('郊山')}>
+                郊山
+              </label>
               <input type="radio" id="oversea" name="category" value="海外" />
-              <label htmlFor="oversea">海外</label>
+              <label htmlFor="oversea" onClick={(e) => setCategory('海外')}>
+                海外
+              </label>
             </div>
             {/* 困難度 */}
             <label className={styles.inputLabel}>困難度</label>
@@ -181,11 +289,26 @@ export default function EditReviewPage() {
               {isNewPost ? (
                 <>
                   <div className={styles.buttonContainer}>
-                    <SecondaryButtonLight>暫存心得</SecondaryButtonLight>
+                    <SecondaryButtonLight onClick={handleScratch}>
+                      暫存心得
+                    </SecondaryButtonLight>
                   </div>
                   <div className={styles.buttonContainer}>
                     <SecondaryButton onClick={handleFormSubmit}>
                       發布心得
+                    </SecondaryButton>
+                  </div>
+                </>
+              ) : isDraft ? (
+                <>
+                  <div className={styles.buttonContainer}>
+                    <SecondaryButtonLight onClick={handleEditScratch}>
+                      暫存草稿
+                    </SecondaryButtonLight>
+                  </div>
+                  <div className={styles.buttonContainer}>
+                    <SecondaryButton onClick={handleFormSubmit}>
+                      發布草稿
                     </SecondaryButton>
                   </div>
                 </>
