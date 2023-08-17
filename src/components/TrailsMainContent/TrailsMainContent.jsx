177ac1c6c74e0dc64ae3d46808f7aef0e4cc99c5
magic9701 <magic9701@gmail.com>
2023-08-17 14:20:34 +0800

    feat: add not match result

--- src/components/TrailsMainContent/TrailsMainContent.jsx
+++ src/components/TrailsMainContent/TrailsMainContent.jsx
@@ -9,8 +9,10 @@ import TrailsListCard from 'components/TrailsList/TrailsListCard/TrailsListCard'
 import CardSkeleton from 'components/Skeleton/CardSkeleton'
 // api
 import { getAllTrails, searchTrailByKeyword } from 'api/trail'
+import YouHaveNothing from 'components/UserContent/YouHaveNothing/YouHaveNothing'
 
-const { container, innerContainer, search, list } = styles
+const { container, innerContainer, search, list, youHaveNothingContainer } =
+  styles
 
 const TrailsMainContent = () => {
   const { keyword } = useParams()
@@ -36,15 +38,21 @@ const TrailsMainContent = () => {
           <TrailsSearchBar type="trail" />
           <FilterToggole />
         </div>
-        <div className={list}>
-          {trailData
-            ? trailData.map((item) => (
-                <TrailsListCard key={item.id} data={item} />
-              ))
-            : Array.from({ length: 5 }).map((_, index) => (
-                <CardSkeleton key={index} />
-              ))}
-        </div>
+        {trailData.length === 0 ? (
+          <div className={youHaveNothingContainer}>
+            <YouHaveNothing robotDescription="沒有符合的搜尋結果" />
+          </div>
+        ) : (
+          <div className={list}>
+            {trailData
+              ? trailData.map((item) => (
+                  <TrailsListCard key={item.id} data={item} />
+                ))
+              : Array.from({ length: 12 }).map((_, index) => (
+                  <CardSkeleton key={index} />
+                ))}
+          </div>
+        )}
       </div>
     </div>
   )
