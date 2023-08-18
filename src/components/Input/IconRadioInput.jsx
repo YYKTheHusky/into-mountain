b417b0975a4c366a0cd84d1d105e9980dffa5d6d
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-08-18 18:37:25 +0800

    fix: fix EditReviewPage

--- src/components/Input/IconRadioInput.jsx
+++ src/components/Input/IconRadioInput.jsx
@@ -11,6 +11,7 @@ export default function IconRadioInput({ iconType, onChange, score }) {
   const [selectedIcon, setSelectedIcon] = useState(null)
   const [defaultIcon, setDefaultIcon] = useState(null)
   const [activeIcon, setActiveIcon] = useState(null)
+  const [icons, setIcons] = useState([])
 
   useEffect(() => {
     if (iconType === 'difficulty') {
@@ -23,46 +24,119 @@ export default function IconRadioInput({ iconType, onChange, score }) {
   }, [iconType])
 
   const handleIconClick = (iconNumber) => {
+    if (iconNumber > 5) {
+      iconNumber = 5
+    }
     setSelectedIcon(iconNumber)
+
     if (onChange) {
       onChange(iconNumber)
     }
   }
 
-  const renderIcons = () => {
-    const icons = []
+  useEffect(() => {
+    if (iconType === 'difficulty') {
+      setDefaultIcon(difficultyIconWhite)
+      setActiveIcon(difficultyIcon)
+    } else if (iconType === 'recommend') {
+      setDefaultIcon(recommendIconWhite)
+      setActiveIcon(recommendIcon)
+    }
+  }, [iconType])
+
+  useEffect(() => {
     const selectedCount = score || selectedIcon || 0
     const unselectedCount = 5 - selectedCount
-
+    const iconElements = []
     for (let i = 1; i <= selectedCount; i++) {
-      icons.push(
+      iconElements.push(
         <div key={`selected-${i}`} onClick={() => handleIconClick(i)}>
           <img
-            className={`${styles.icon} ${!score && styles.iconHover}`}
+            className={`${styles.icon} ${styles.iconHover}`}
             src={activeIcon}
             alt="active icon"
           />
         </div>
       )
     }
-
     for (let i = 1; i <= unselectedCount; i++) {
-      icons.push(
+      iconElements.push(
         <div
           key={`unselected-${i}`}
           onClick={() => handleIconClick(selectedCount + i)}
         >
           <img
-            className={`${styles.icon} ${!score && styles.iconHover}`}
+            className={`${styles.icon} ${styles.iconHover}`}
             src={defaultIcon}
             alt="default icon"
           />
         </div>
       )
     }
+    setIcons(iconElements)
+  }, [score, selectedIcon, activeIcon, defaultIcon])
 
-    return icons
-  }
-
-  return <div className={styles.iconInput}>{renderIcons()}</div>
+  return <div className={styles.iconInput}>{icons}</div>
 }
+
+// --- 原版
+// export default function IconRadioInput({ iconType, onChange, score }) {
+//   const [selectedIcon, setSelectedIcon] = useState(null)
+//   const [defaultIcon, setDefaultIcon] = useState(null)
+//   const [activeIcon, setActiveIcon] = useState(null)
+
+//   useEffect(() => {
+//     if (iconType === 'difficulty') {
+//       setDefaultIcon(difficultyIconWhite)
+//       setActiveIcon(difficultyIcon)
+//     } else if (iconType === 'recommend') {
+//       setDefaultIcon(recommendIconWhite)
+//       setActiveIcon(recommendIcon)
+//     }
+//   }, [iconType])
+
+//   const handleIconClick = (iconNumber) => {
+//     setSelectedIcon(iconNumber)
+
+//     if (onChange) {
+//       onChange(iconNumber)
+//     }
+//   }
+
+//   const renderIcons = () => {
+//     const icons = []
+//     const selectedCount = score || selectedIcon || 0
+//     const unselectedCount = 5 - selectedCount
+//     console.log(selectedCount)
+//     for (let i = 1; i <= selectedCount; i++) {
+//       icons.push(
+//         <div key={`selected-${i}`} onClick={() => handleIconClick(i)}>
+//           <img
+//             className={`${styles.icon} ${!score && styles.iconHover}`}
+//             src={activeIcon}
+//             alt="active icon"
+//           />
+//         </div>
+//       )
+//     }
+
+//     for (let i = 1; i <= unselectedCount; i++) {
+//       icons.push(
+//         <div
+//           key={`unselected-${i}`}
+//           onClick={() => handleIconClick(selectedCount + i)}
+//         >
+//           <img
+//             className={`${styles.icon} ${!score && styles.iconHover}`}
+//             src={defaultIcon}
+//             alt="default icon"
+//           />
+//         </div>
+//       )
+//     }
+
+//     return icons
+//   }
+
+//   return <div className={styles.iconInput}>{renderIcons()}</div>
+// }
