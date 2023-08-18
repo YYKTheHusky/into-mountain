346c74a97731914acec9321e600ad6d80b7f8fa4
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-08-18 19:50:29 +0800

    feat: add MyReview draft feature

--- src/components/UserContent/MyReview/ListItem/ListItem.jsx
+++ src/components/UserContent/MyReview/ListItem/ListItem.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom'
 
 const {
   listItem,
+  inProgressStyle,
+  draftStyle,
   itemRight,
   itemLeft,
   itemRightTitle,
@@ -14,28 +16,32 @@ const {
   titleButton2
 } = styles
 
-const ListItem = ({ data, theUserId, onDeletePost }) => {
+const ListItemContent = ({ data, theUserId, onDeletePost }) => {
   const navigate = useNavigate()
-
   return (
-    <div className={listItem} onClick={() => navigate(`/review/${data.id}`)}>
+    <>
       <div className={itemLeft}>
         <img src={data.image} alt="" />
       </div>
       <div className={itemRight}>
         <div className={itemRightTitle}>
-          <div className={titleText}>{data.title}</div>
+          <div className={titleText}>
+            <div>{data.title}</div>
+            {data.inProgress && <div className={draftStyle}>(草稿)</div>}
+          </div>
           {theUserId === localStorage.getItem('currentUserId') && (
             <div className={btnGroup}>
-              <button
-                className={titleButton1}
-                onClick={(event) => {
-                  event.stopPropagation()
-                  navigate(`/review/${data.id}/edit`)
-                }}
-              >
-                編輯
-              </button>
+              {!data.inProgress && (
+                <button
+                  className={titleButton1}
+                  onClick={(event) => {
+                    event.stopPropagation()
+                    navigate(`/review/${data.id}/edit`)
+                  }}
+                >
+                  編輯
+                </button>
+              )}
               <button
                 className={titleButton2}
                 onClick={(event) => {
@@ -56,8 +62,51 @@ const ListItem = ({ data, theUserId, onDeletePost }) => {
           發表於：{formatDateWithTime(data.updatedAt)}
         </div>
       </div>
-    </div>
+    </>
   )
+}
+
+const ListItem = ({ data, theUserId, onDeletePost }) => {
+  const navigate = useNavigate()
+  if (theUserId === localStorage.getItem('currentUserId')) {
+    return (
+      <>
+        {data.inProgress ? (
+          <div
+            className={`${listItem} ${inProgressStyle}`}
+            onClick={() => navigate(`/review/${data.id}/edit`)}
+          >
+            <ListItemContent
+              data={data}
+              theUserId={theUserId}
+              onDeletePost={onDeletePost}
+            />
+          </div>
+        ) : (
+          <div
+            className={listItem}
+            onClick={() => navigate(`/review/${data.id}`)}
+          >
+            <ListItemContent
+              data={data}
+              theUserId={theUserId}
+              onDeletePost={onDeletePost}
+            />
+          </div>
+        )}
+      </>
+    )
+  } else {
+    return (
+      <div className={listItem} onClick={() => navigate(`/review/${data.id}`)}>
+        <ListItemContent
+          data={data}
+          theUserId={theUserId}
+          onDeletePost={onDeletePost}
+        />
+      </div>
+    )
+  }
 }
 
 export default ListItem
